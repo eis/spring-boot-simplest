d43e7937b708b812db5c5008aa2c7f097c5911ad
eis <eis@iki.fi>
2017-03-17 14:42:20 +0200

    dockerize

--- README.txt
+++ README.txt
@@ -9,3 +9,7 @@ class HelloworldController {
 $ spring run Helloworld.groovy
 $ curl localhost:8080
 Hello world!
+
+$ spring jar app.jar Helloworld.groovy
+$ docker build -t hello .
+$ docker run -ti --rm hello
--- ubuntu-deps.sh
+++ ubuntu-deps.sh
@@ -3,7 +3,8 @@ set -e
 set -o pipefail
 
 sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk unzip zip
+sudo apt-get install -y openjdk-8-jdk unzip zip docker.io
+sudo usermod -aG docker ubuntu
 curl -s "https://get.sdkman.io" | bash
 source "/home/ubuntu/.sdkman/bin/sdkman-init.sh"
 sdk install springboot
